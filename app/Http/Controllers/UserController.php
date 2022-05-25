79da920e56b8720b9b2b980fd846290f8e673617
AHMED <programmer@octopusdigitalnetwork.com>
2022-05-25 16:47:14 +0500

    .

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -176,19 +176,20 @@ class UserController extends Controller
         $body = $_SERVER['SERVER_NAME'] . "/activation/$randomString";
         $data = array("name" => $to_name, "body" => $body);
 
-        $template = 'confirm-email';
-        $content = EmailHandler::prepareData(EmailHandler::getTemplateContent($template));
-        
-        (new MailMessage)
-        ->view(['html' => new HtmlString($content)])
-        ->subject(EmailHandler::getTemplateSubject($template));
 
-        // Mail::send("activation", $data, function ($message) use ($to_name, $to_email) {
-        //   $message->to($to_email, $to_name)
-        //     ->view(['html' => new HtmlString($content)])
-        //     ->subject("Account Activation Email Makanumber.com");
-        //   $message->from("noreply@makanumber.com", "Makanumber");
-        // });
+        // (new MailMessage)
+        // ->view(['html' => new HtmlString($content)])
+        // ->subject(EmailHandler::getTemplateSubject($template));
+
+        Mail::send("activation", $data, function ($message) use ($to_name, $to_email) {
+          $template = 'confirm-email';
+          $content = EmailHandler::prepareData(EmailHandler::getTemplateContent($template));
+
+          $message->to($to_email, $to_name)
+            ->view(['html' => new HtmlString($content)])
+            ->subject("Account Activation Email Makanumber.com");
+          $message->from("noreply@makanumber.com", "Makanumber");
+        });
 
             // ->subject(EmailHandler::getTemplateSubject($template));
 
