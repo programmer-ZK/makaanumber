5a4db1925124eef0c1a2355f60f6d68b1567609f
AHMED <programmer@octopusdigitalnetwork.com>
2022-05-27 11:33:28 +0500

    .

--- public/js/custom.js
+++ public/js/custom.js
@@ -7,12 +7,14 @@ $(document).ready(function() {
     allowClear: true
   });
   
+  $('#multipleSelect').on('select2:unselect', function(e){
+    $(".select2-selection").css('padding-top', '3px');
+  });
+
   $('#multipleSelect').on('select2:close', function(e){
     $(".select2-selection").css('padding-top', '3px');
     $(".select2-selection__choice:not(:first-child").hide();
     var selectedValues = $("#multipleSelect").select2('val');
-    console.log(selectedValues);
-    console.log(selectedValues.length);
     
     var tag = `<li class="select2-selection__choice" title="${selectedValues.length - 1} More" data-select2-id="select2-data-75-kal7"><span class="select2-selection__choice__display" id="select2-multipleSelect-container-choice-mdng-1">+${selectedValues.length - 1} More </span></li>`;
 
