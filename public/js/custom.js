a9039522f713b0211fc233c61dd962b902616b05
AHMED <programmer@octopusdigitalnetwork.com>
2022-06-03 16:35:15 +0500

    .

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,25 +1,25 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
 
   $("#multipleSelect").show();
 
   $("#multipleSelect").select2({
-    width:"100%",
+    width: "100%",
     placeholder: " Emirates/Locations",
   });
 
 
-  $('#multipleSelect').on('select2:close', function(e){
+  $('#multipleSelect').on('select2:close', function (e) {
     $(".select2-selection").css('padding-top', '3px');
     $(".select2-selection__choice:not(:first-child").hide();
     var selectedValues = $("#multipleSelect").select2('val');
-    
+
     var tag = `<li class="select2-selection__choice" title="${selectedValues.length - 1} More" data-select2-id="select2-data-75-kal7"><span class="select2-selection__choice__display" id="select2-multipleSelect-container-choice-mdng-1">+${selectedValues.length - 1} More </span></li>`;
 
 
-    if(selectedValues.length > 1 ){
+    if (selectedValues.length > 1) {
       $("#select2-multipleSelect-container").append(tag);
-    }else{
+    } else {
       $("#select2-multipleSelect-container").append('');
 
     }
@@ -29,16 +29,16 @@ $(document).ready(function() {
 
   });
 
-  $("#searchFormSubmit").on('click', function(e){
+  $("#searchFormSubmit").on('click', function (e) {
     // e.preventDefault();
     var selectedValues = $("#multipleSelect").select2('val');
     console.log(selectedValues);
-    
+
   });
 
 
-  
-   
+
+
   // $(document).click("#multiple-select", function(){
   //   console.log('clicked!');
   // });
@@ -59,19 +59,5 @@ $(document).ready(function() {
   //   // Do something
   // });
 
-  function returnPromises() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      $("#searchSpinner").hide();
-      resolve("Sample Data");
-    }, 3000);
-  });
-}
-
-async function ExecuteFunction() {
-  await returnPromises();
-  $("#searchItems").fadeIn();
-}
-
-ExecuteFunction()
+  
 });
